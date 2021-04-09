429fe3a6a851df6b0f17e8147740957a7da2f70a
Caia Chuang <caiachuang@gmail.com>
2021-04-09 16:50:27 -0700

    Add routes to App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Upload from './pages/Upload';
 
 const App = () => {
     return (
-        <div className="App">
-        
-        </div>
+        <HashRouter>
+            <Switch>
+                <Route path="/upload" component={Upload} />
+                <Route path="/" component={Home} />
+            </Switch>
+        </HashRouter>
     );
 }
 
