72de9a1e5090a8615d0f4e559f8d23fed733d2a8
Caia Chuang <caiachuang@gmail.com>
2021-04-09 17:17:36 -0700

    Add some styles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Header from './components/Header';
 import Home from './pages/Home';
 import Upload from './pages/Upload';
 
 const App = () => {
     return (
         <HashRouter>
+            <Header />
             <Switch>
                 <Route path="/upload" component={Upload} />
                 <Route path="/" component={Home} />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import './index.css';
+
 import App from './App';
 
 ReactDOM.render(
